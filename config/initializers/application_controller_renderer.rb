579443e6217913e215afa6457e1c80358254f66b
HidekiShiba <hshiba033@gmail.com>
2020-11-12 11:26:45 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd7e6266c7d538a2e0a0cff61eb6b6599b09f584e00d977bf4d8e84739ee8e943c1c4382ecbd3583302ec2226d82dd39230bbab3a66b14ee7c6a76b87bb035d69'
